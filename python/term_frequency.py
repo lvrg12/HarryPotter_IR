ba90ec665ad5d540618f0689cb19e6c09b136758
Lino <lino.virgen96@gmail.com>
2018-09-18 18:17:59 -0500

    term frequency update

--- python/term_frequency.py
+++ python/term_frequency.py
@@ -1,5 +1,6 @@
 import re
 
+# returns list of documents where term appears
 def documents_with(term,documents):
     docs = []
 
@@ -9,7 +10,7 @@ def documents_with(term,documents):
 
     return docs
 
-
+# returns list of position where term appears in doc
 def document_positions(doc,term):
 
     return [i for i, x in enumerate(doc) if x == term]
