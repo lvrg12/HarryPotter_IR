7682e2bb31ebf763d9672197e7fbdcf2cb48469c
Lino Virgen <lino.virgen96@gmail.com>
2018-11-09 00:09:54 -0600

    pc

--- python/main3.py
+++ python/main3.py
@@ -13,7 +13,7 @@ def main():
 
     # 1. Pre-processing
     doc = {}
-    # N = 0
+    N = 0
     # for name in sorted(os.listdir("../dataset2/pdfs")):
 
     #     if( name == ".DS_Store" or name == "hp3_22.pdf" or name == "hp4_37.pdf"):
@@ -76,6 +76,29 @@ def main():
     # 5. K-Means Clustering
     # print(cl.clust(vector_space,5))
 
+    # 4. P(c)
+    with open('../locations.csv', newline='') as file:
+        reader = csv.reader(file)
+        classes = {}
+        total = 0
+        for row in reader:
+
+            if( row[2] == "place" ):
+                continue
+
+            total = total + 1
+            if row[2] in classes:
+                classes[row[2]] = classes[row[2]] + 1
+            else:
+                classes[row[2]] = 1
+    
+    p_c = {}
+    for c in classes:
+        p_c[c] = classes[c] / total
+        print( "P(" + c + ") =\t\t\t" + str(classes[c]) + "/" + str(total))
+
+    print(p_c)
+
 def normalized( vector ):
 
     sum = 0
@@ -86,69 +109,6 @@ def normalized( vector ):
         return vector
     else:
         return [ c/sum for c in vector ]
-
-
-
-    
-
-    # a) Term Frequency
-    # print("Term Frequency")
-    # for t in query:
-    #     print(t, end="  ")
-    #     for d in pos[t]:
-    #         print( str(d) + ": " + str(tfidf.tf(t,d,pos)) + " ", end=" ")
-    #     print()
-
-    # arrtmp = [0] * 130
-    # for t in query:
-    #     print(t, end=",")
-    #     for d in range(len(arrtmp)):
-    #         if d in pos[t]:
-    #             print( str(tfidf.tf(t,d,pos)), end="," )
-    #         else:
-    #             print( end="0," )
-    #     print()
-
-    # max_f = {}
-    # for t in query:
-    #     for d in pos[t]:
-    #         max_f[t] = len(pos[t][d])
-
-
-
-    # # a) Inverse Document Frequency
-    # # print("\nInverse Document Frequency")
-    # # for t in query:
-    # #     print(t + "," + str( tfidf.idf(len(doc),t,pos) ) )
-
-    # # 4. TF-IDF
-    # # print("\nTF-IDF")
-    # # for d in doc:
-    # #     print( str( tfidf.tfidf(len(doc),d,query,pos) ) )
-
-    # # 6. Vector Space
-    # vector_space = {}
-    # # print("\nVector Space")
-    # for d in doc:
-    #     vector_space[d] = vsm.vector(d,query,pos,len(doc),max_f)
-    #     # print( str(d) + ": " + str( vector[d] ) )
-
-    # # 7. Cosine Similarity
-    # # print("\nCosine Similarity")
-    # vector_space = vsm.normalize_vector(vector_space)
-
-    # cos_sim_matrix = []
-    # i = 0
-    # for d in doc:
-    #     cos_sim_matrix.append([])
-    #     for d2 in doc:
-    #         sim = round(vsm.cosine_similarity(vector_space[d],vector_space[d2]),3)
-    #         cos_sim_matrix[i].append(sim)
-    #         # print(sim, end=",")
-    #     # print()
-    #     i = i + 1
-
-    # print(cl.clust(cos_sim_matrix,5))
 
 
 if __name__ == "__main__":
